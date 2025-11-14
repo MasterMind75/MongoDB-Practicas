30a9fc04c8966c6a61651ce95fbfe4d09c9c6852
MasterMind75 <ramirez.lugo.ethan.46@cbtis118.edu.mx>
2025-11-14 17:26:56 +0000

    Se agregan los ejercicios de la practica 1.

---
+++ Practica1.mongodb.js
@@ -0,0 +1,8 @@
+use("escuela");
+db.createCollection("alumnos");
+db.createCollection("profesores");
+db.createCollection("cursos");
+db.createCollection("grupos");
+db.createCollection("administrativos");
+
+db.getCollectionNames();
